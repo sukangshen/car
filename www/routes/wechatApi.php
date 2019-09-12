c453acf21ede714246f4d35e19b70e5588056b61
kangshen su <kangshensu@gmail.com>
2019-09-12 15:48:46 +0800

    授权不需要token

--- www/routes/wechatApi.php
+++ www/routes/wechatApi.php
@@ -14,18 +14,16 @@ Route::group(['middleware' => 'api.jwt.auth'], function (){
     Route::post('logout', 'UserController@logout');
     Route::post('me', 'UserController@me');
     Route::post('refresh', 'UserController@refresh');
-    //微信
-    Route::any('auth', 'UserController@auth');
-    Route::any('callback', 'UserController@callback');
-
-    //微信授权相关
-    Route::any('auth', 'UserController@auth');
-    Route::any('callback', 'UserController@callback');
 
     //七牛云图片上传
     Route::post('img', 'UploadController@img');
 
 });
+
+//微信授权
+Route::any('auth', 'UserController@auth');
+Route::any('callback', 'UserController@callback');
+
 
 //获取城市列表
 Route::get('getAddressList', 'AddressController@getAddressList');
