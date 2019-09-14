c8e8b6ba1f26f9fd0cae9eb4679af170f19969e4
kangshen su <kangshensu@gmail.com>
2019-09-14 12:57:42 +0800

    增加校验发布权限

--- www/app/Http/Controllers/Api/ProfileController.php
+++ www/app/Http/Controllers/Api/ProfileController.php
@@ -18,6 +18,20 @@ class ProfileController extends Controller
 {
 
     /**
+     * Desc:校验帖子
+     * User: kangshensu@gmail.com
+     * Date: 2019-09-14
+     */
+    public function profileCreateCheck(Request $request)
+    {
+        $user = auth('api')->user();
+        //判断是否有未下架的帖子
+        $exist = Profile::query()->where('end_time', '>', time())->where('user_id', $user['id'])->first();
+
+        return $this->success(intval($exist));
+    }
+
+    /**
      * Desc:创建
      * User: kangshensu@gmail.com
      * Date: 2019-09-12
@@ -27,18 +41,20 @@ class ProfileController extends Controller
     public function profileCreate(Request $request)
     {
         $params = $request->all();
-        $params['end_time'] = time() + 24 * 60 * 60 * Profile::END_TIME;
 
-        //增加资源
+        //增加图片资源
         if (empty($params['wechat_img']) || empty($params['self_img'])) {
             return $this->fail(400);
         }
+
         $user = auth('api')->user();
         Log::info('用户信息' . print_r($user, true));
 
+        $params['nickname'] = trim($user['nickname']);
+        $params['end_time'] = time() + 24 * 60 * 60 * Profile::END_TIME;
+
         //增加资源
         $resourceParams['user_id'] = $user['id'];
-        $resourceParams['nickname'] = trim($user['nickname']);
         $resourceImg = ['wechat_img' => $params['wechat_img'], 'self_img' => $params['self_img']];
         $resourceParams['resource'] = json_encode($resourceImg);
         $resourceCreate = Resources::query()->create($resourceParams);
