cfa5c4bf4bf14bbf050e924e396a98d2cd7d4fa0
kangshen su <kangshensu@gmail.com>
2019-09-15 14:38:19 +0800

    增加我的

--- www/app/Http/Controllers/Api/MeController.php
+++ www/app/Http/Controllers/Api/MeController.php
@@ -7,8 +7,44 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Profile;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Controller as Controller;
 
-class MeController
+class MeController extends Controller
 {
+
+    /**
+     * Desc:获取正在进行中的帖子
+     * User: kangshensu@gmail.com
+     * Date: 2019-09-15
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function myProfile(Request $request)
+    {
+        $user = auth('api')->user();
+        //获取个人正在进行中的帖子
+        $profile = Profile::query()->where('end_time', '>', time())->where('user_id', $user['id'])->get();
+        return $this->success($profile);
+    }
+
+    public function myOrder(Request $request)
+    {
+
+    }
+
+    /**
+     * Desc:获取个人的信息
+     * User: kangshensu@gmail.com
+     * Date: 2019-09-15
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function userInfo(Request $request)
+    {
+        $user = auth('api')->user();
+        return $this->success($user);
+    }
 
 }
--- www/routes/wechatApi.php
+++ www/routes/wechatApi.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Desc:
@@ -9,7 +8,7 @@
 /**
  * 引入Auth中间件 验证是否授权
  */
-Route::group(['middleware' => 'api.jwt.auth'], function (){
+Route::group(['middleware' => 'api.jwt.auth'], function () {
     //Token
     Route::post('logout', 'UserController@logout');
     Route::post('me', 'UserController@me');
@@ -21,16 +20,29 @@ Route::group(['middleware' => 'api.jwt.auth'], function (){
     //校验发布权限
     Route::get('profileCreateCheck', 'ProfileController@profileCreateCheck');
 
+    //获取我的信息
+    Route::get('userInfo', 'MeController@userInfo');
+
+//    //获取我发布的帖子
+//    Route::get('myProfile', 'MeController@myProfile');
+
 });
 
-//微信授权
+//微信授权入口
 Route::any('auth', 'UserController@auth');
+
+//微信授权回调
 Route::any('callback', 'UserController@callback');
 
 
 //获取城市列表
 Route::get('getAddressList', 'AddressController@getAddressList');
+
 //获取帖子列表
 Route::get('profileSearch', 'ProfileController@profileSearch');
+
 //七牛云图片上传
 Route::any('img', 'UploadController@img');
+
+//获取我发布的帖子
+Route::get('myProfile', 'MeController@myProfile');
